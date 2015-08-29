f36717e3f0176595aae0fb9f569a358f9312a669
Michael Marner <michael@20papercups.net>
2015-08-30 00:41:44 +0930

    Tewaking grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,8 +55,8 @@ module.exports = function(grunt) {
         uglify: {
             build: {
                 options: {
-                    mangle: false,
-                    beautify: true,
+                    mangle: true,
+                    beautify: false,
                 }
                 ,
                 files: {
